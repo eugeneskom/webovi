9a8be3f1a8e0e27d837b085677fc84c4ac548bb4
eugeneskom <55525928+eugeneskom@users.noreply.github.com>
2021-09-01 11:45:17 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -30,4 +30,114 @@ $(function () {
 
     ]
   });
+
+
+
+
+
+
+  // GSAP ANIMATION
+
+  let gsapLeft = document.querySelectorAll('.gsap-left');
+  let gsapRight = document.querySelectorAll('.gsap-right');
+  let gsapPerson = document.querySelectorAll('.gsap-person');
+
+  gsap.registerPlugin(ScrollTrigger);
+
+
+
+
+
+  for (let i = 0; i < gsapLeft.length; i++) {
+    gsap.from(gsapLeft[i], {
+      scrollTrigger: {
+        trigger: gsapLeft[i],
+        start: 'top 80%',
+        end: 'top 70%',
+        scrub: 3,
+        toggleActions: 'restart none none restart',
+      },
+      // stagger: 0.1,
+      ease: 'Back.easeOut',
+      opacity: 0,
+      xPercent: -100,
+      duration: 2,
+    });
+
+  }
+
+
+  for (let i = 0; i < gsapRight.length; i++) {
+    gsap.from(gsapRight[i], {
+      scrollTrigger: {
+        trigger: gsapRight[i],
+        start: 'top 80%',
+        end: 'top 70%',
+        scrub: 3,
+        toggleActions: 'restart none none restart',
+      },
+      // stagger: 0.1,
+      ease: 'Back.easeOut',
+      opacity: 0,
+      xPercent: 100,
+      duration: 2,
+    });
+
+  }
+
+  for (let i = 0; i < gsapPerson.length; i++) {
+    gsap.from(gsapPerson[i], {
+      scrollTrigger: {
+        trigger: gsapPerson[i],
+        start: 'top 80%',
+        end: 'top 70%',
+        scrub: 3,
+        toggleActions: 'restart none none restart',
+      },
+      // stagger: 0.1,
+      ease: 'Back.easeOut',
+      opacity: 0,
+      duration: 2,
+      delay: 1,
+    });
+
+  }
+
+  gsap.from('.about__person', {
+    scrollTrigger: {
+      trigger: '.about__person',
+      start: 'top 80%',
+      end: 'top 70%',
+
+      scrub: 3,
+      toggleActions: 'restart none none restart',
+    },
+    opacity: 0,
+    duration: 1
+  })
+
+gsap.from('.footer__title',{
+  scrollTrigger: {
+    trigger: '.footer__title',
+    start: 'top 90%',
+    toggleActions: 'restart none none play',
+    markers: true,
+  },
+  opacity: 0,
+  duration: 1,
+  ease: 'Back.easeIn',
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
